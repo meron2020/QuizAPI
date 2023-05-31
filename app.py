3fb817acbe72f997b300fa76a5e04c0a661d7f54
yoavm <yoav.meron11@gmail.com>
2023-05-31 21:29:40 +0300

    commit

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api
-from api.resources.quiz import Quiz, checker
+from api.resources.quiz import Quiz
 from api.resources.quiz import QuizList
 from datetime import timedelta
 from db import db
 from api.resources.user import User
-#from flask_jwt import JWT
 from flask_cors import CORS
 from api.resources.scores import Scoring
-from security import authenticate, identity
 from api.resources.auth import Auth
 from api.resources.picture import Picture
 
@@ -23,26 +21,20 @@ CORS(app)
 api = Api(app)
 
 
-#jwt = JWT(app, authenticate, identity)
-
-
-#
-
 @app.before_first_request
 def create_tables():
     db.init_app(app)
     db.create_all()
 
 
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+# @app.route('/')
+# def serve():
+#     return send_from_directory(app.static_folder, 'index.html')
 
 
 api.add_resource(Quiz, '/quiz/<string:name>')
 api.add_resource(QuizList, '/quizzes')
 api.add_resource(User, '/user/<string:name>')
-api.add_resource(checker, '/checker')
 api.add_resource(Auth, '/auth')
 api.add_resource(Scoring, '/scoring')
 api.add_resource(Picture, '/pictures/<string:name>')
